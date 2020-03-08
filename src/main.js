29bfaf6bff5c46abe815563b14b0abde212b291b
John Kester <nnamanikester@gmail.com>
2020-03-08 10:56:00 +0100

    Added: Vue-Meta Using yarn

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueMeta from "vue-meta"
 import "./registerServiceWorker";
 import "@/components";
 import Fragment from "vue-fragment";
@@ -8,6 +9,7 @@ import VueSkeletonLoading from "vue-skeleton-loading";
 
 Vue.use(Fragment.Plugin);
 Vue.use(VueSkeletonLoading);
+Vue.use(VueMeta)
 
 Vue.config.productionTip = false;
 
